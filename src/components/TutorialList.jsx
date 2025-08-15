f20cc55abf3dcd4f3ed60fa992a53169b554d32c
Nursaadet <nursaadetozdemir@gmail.com>
2025-08-15 15:48:11 +0300

    feat: add Bootstrap modal component (EditModal)

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,6 +1,7 @@
 import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 import axios from "axios";
+import EditModal from "./EditModal";
 
 const TutorialList = ({ tutorials, getTutorials }) => {
   // const tutorials =
@@ -62,6 +63,7 @@ const TutorialList = ({ tutorials, getTutorials }) => {
           })}
         </tbody>
       </table>
+      <EditModal />
     </div>
   );
 };
